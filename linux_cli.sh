09f66ceee84c26e804bfe98da900a1d638430372
gafinos <gafinos@gmail.com>
2025-10-13 18:10:15 +0200

    Add functions

--- linux_cli.sh
+++ linux_cli.sh
@@ -1,4 +1,56 @@
-echo -e "\033[0;31mWelcome to Task 3\033[1m\033[0m\nShell used: $SHELL\n"
-whoami
-cat /etc/os-release
-env
+#! /bin/bash
+
+printUser() {
+	echo "User is: "
+	whoami
+}
+
+printOsVersion() {
+	echo "OS Version: "
+	cat /etc/os-release
+}
+
+printShellUsed() {
+	echo "Shell used: $SHELL"
+}
+
+logError() {
+	echo "Error: $1" >&2
+}
+
+printEnvs() {
+	echo "All environment variables: "
+	env
+}
+
+log() {
+	echo "======= $1 ======="
+}
+
+
+logError() {
+	echo "$1" >&2
+}
+
+listUpgradablePackages() {
+	apt list --upgradable
+}
+
+upgradePackages() {
+	apt-get update && apt-get upgrade
+}
+
+addLink() {
+	ln -s $(dirname "$0")/$(basename "$0") /bin/linux_cli/$(basename "$0")
+}
+log("Welcome to Task 4\n")
+
+printShellUsed
+printUser
+printOsVersion
+printEnvs
+
+logError "O oh!"
+
+export MY_ENV_VAR=1
+OS_VERSION=($cat /etc/os-release
